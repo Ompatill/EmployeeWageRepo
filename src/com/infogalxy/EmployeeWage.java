031f06fdcf513782c9398399f24964563f92ee38
Ompatill <ompatill2004@gmail.com>
2022-03-18 12:03:54 +0530

    MODIFIED: Calculating Employee's Wage for a Month

--- src/com/infogalxy/EmployeeWage.java
+++ src/com/infogalxy/EmployeeWage.java
@@ -10,32 +10,35 @@ public class EmployeeWage {
         final int WAGE_PER_HR = 20;
         final int PART_TIME_HR = 4;
         final int FULL_DAY_HR = 8;
-        int total_wage = 0;
         Random random = new Random();
-        //UC-1: Checking Employee Attendance
-        int empAttendance = random.nextInt()%2;
-        System.out.println(empAttendance);
-        if(empAttendance == 0) {
-            System.out.println("Employee is Absent");
-            total_wage = 0;
-        }else {
-            System.out.println("Employee is Present");
-            //UC-3: ADD Part Time Employee And Wage
-            int empType = random.nextInt() %2 ;
-            //UC-4:Solving Using Switch Case Statement
-            switch(empType) {
-                case 0:
-                    System.out.println("Employee is Part Time");
-                    total_wage = WAGE_PER_HR * PART_TIME_HR;
-                    break;
-                case 1:
-                case -1:
-                    System.out.println("Employee is Full Time");
-                    total_wage = WAGE_PER_HR * FULL_DAY_HR;
-                    break;
-            }
+        //UC-5:Calculating Wages for 20days
+        for(int i = 1; i <= 20; i++) {
+            int total_wage = 0;
+            //UC-1:Checking Employee's Attendance
+            int empAttendance = random.nextInt() % 2;
+            System.out.println(empAttendance);
+            if (empAttendance == 0) {
+                System.out.println("Employee is Absent");
+                total_wage = 0;
+            } else {
+                System.out.println("Employee is Present");
+                //UC-3: ADD Part Time Employee And Wage
+                int empType = random.nextInt() % 2;
+                //UC-4:Solving Using Switch Case Statement
+                switch (empType) {
+                    case 0:
+                        System.out.println("Employee is Part Time");
+                        total_wage = WAGE_PER_HR * PART_TIME_HR;
+                        break;
+                    case 1:
+                    case -1:
+                        System.out.println("Employee is Full Time");
+                        total_wage = WAGE_PER_HR * FULL_DAY_HR;
+                        break;
+                }
 
+            }
+            System.out.println("Employee's Full Day Wage:" + total_wage);
         }
-        System.out.println("Employee's Full Day Wage:"+total_wage);
     }
 }
